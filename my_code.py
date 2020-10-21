61aa30bc264afcb672db76a2328297a4050bbb5b
23aartale <23aartale@athenian.org>
2020-10-21 19:29:56 +0000

    updated

--- my_code.py
+++ my_code.py
@@ -1,3 +1,16 @@
 # Collaborators (including web sites where you got help: (enter none if you didn't need help)
 #  
+def sports_dict():
+    sports_book = {}
+    while True:
+        team = input("Say a football team- ")
+        player = input("Say their best player- ")
+        tries = input("if your done type y, if not no: ")
+        if tries == 'y':
+            break
+        sports_book[team] = player
+        for x, y in sports_book.items(): 
+            print(x,y)
 
+
+sports_dict()
